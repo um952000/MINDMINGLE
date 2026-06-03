9110bbc66cbd94942b0e2504512134b6998f55d0
Utkarsh Mishra <um952000@gmail.com>
2026-06-03 21:58:52 +0530

    set

--- mindmingle-backend/mindmingle/settings.py
+++ mindmingle-backend/mindmingle/settings.py
@@ -203,7 +203,7 @@ CHANNEL_LAYERS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://mindmingle-one.vercel.app/",
+    "https://mindmingle-one.vercel.app",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
