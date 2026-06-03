24dd8450dec92c4989013d29c85a6e2aa1ca95e1
Utkarsh Mishra <um952000@gmail.com>
2026-06-03 21:48:56 +0530

    vercel

--- mindmingle-backend/mindmingle/settings.py
+++ mindmingle-backend/mindmingle/settings.py
@@ -203,7 +203,7 @@ CHANNEL_LAYERS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    # "https://your-app.vercel.app",  # ← uncomment after React deploy
+    "https://mindmingle-one.vercel.app/",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
